2f072ee89cb7bbb5b172435c99ade3b1f57f5d50
Jim Ladd <jladdjr@gmail.com>
2022-02-05 16:00:48 -0600

    flesh out today report a bit more

--- chomp.py
+++ chomp.py
@@ -67,17 +67,27 @@ def today(args):
     food_diary = get_food_diary()
     start_of_day = get_beginning_of_day_timestamp()
 
+    print(f'      time_of_day    |       food       |    calories  ')
+    print(f'---------------------------------------------------------')
     calorie_total = 0
     for timestamp in food_diary:
-        print(f'found entry for {timestamp}')
+        #print(f'found entry for {timestamp}')
         if int(timestamp) < start_of_day:
-            print(' before today.. skipping')
+            #print(' before today.. skipping')
             continue
         entry = food_diary[timestamp]
         if 'calories' not in entry:
             print(' missing calorie information.. skipping')
             continue
+
+        time_of_day = datetime.fromtimestamp(int(timestamp))
+        calories = entry['calories']
+        food = entry['food']
+        info_line = f' {time_of_day}         {food:15}   {calories}'
+        print(info_line)
+
         calorie_total += entry['calories']
+    print()
     print(f'Total calories for the day: {calorie_total}')
 
 def main():
