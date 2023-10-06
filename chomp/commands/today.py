73887a6856f739c4dcf2ec545f3bd5669df22bbf
Jim Ladd <jladd@tuta.io>
2023-10-06 15:33:54 -0700

    use tabulate to create tables

--- chomp/commands/today.py
+++ chomp/commands/today.py
@@ -1,6 +1,8 @@
 from copy import copy
 from datetime import datetime
 
+from tabulate import tabulate
+
 from chomp.food import Food
 from chomp.data_manager import (
     get_food_diary,
@@ -12,18 +14,13 @@ def today():
     food_diary = get_food_diary()
     start_of_day = get_beginning_of_day_timestamp()
 
-    print(
-        f"      time_of_day    |                   food               |    calories  |    fat"
-    )
-    print(
-        f"----------------------------------------------------------------------------------------"
-    )
+    lines = [['Time of Day', 'Food', 'Calories', 'Fat']]
+
     combined_intake = None
     for timestamp in food_diary:
-        # print(f'found entry for {timestamp}')
         if int(timestamp) < start_of_day:
-            # print(' before today.. skipping')
             continue
+
         entry = food_diary[timestamp]
         if "food" not in entry:
             print(" missing food diary data for entry.. skipping")
@@ -34,13 +31,17 @@ def today():
         name = food.name
         calories = food.get_nutritional_fact('calories')
         fat = food.get_nutritional_fact('fat.total') or 0
-        info_line = f"{time_of_day}    {name:39}   {calories:13}   {fat:^7}"
-        print(info_line)
+        lines.append([time_of_day, name, calories, fat])
 
         if combined_intake is None:
             combined_intake = food
         else:
             combined_intake += food
-    print()
-    print(f"Total calories for the day: {combined_intake.get_nutritional_fact('calories')}")
+    print(tabulate(lines, headers="firstrow", tablefmt="rounded_outline"))
 
+    print()
+
+    lines = [['Total Calories', 'Total Fat']]
+    lines.append([combined_intake.get_nutritional_fact('calories'),
+                  combined_intake.get_nutritional_fact('fat.total')])
+    print(tabulate(lines, headers="firstrow", tablefmt="rounded_outline"))
