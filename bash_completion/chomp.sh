532b1f5be621fa8e64b35f7c95ce7c42651c8c5d
Jim Ladd <j@portladds.com>
2024-08-14 06:30:50 -0700

    Update yq command (in comments for the moment)

--- bash_completion/chomp.sh
+++ bash_completion/chomp.sh
@@ -8,6 +8,8 @@ function _chomp () {
 	#prev="${COMP_WORDS[COMP_CWORD-1]}"
 
 	foods=$(yq '[.[].name] | @csv' ~/.chomp/food_library.yml | tr ',' ' ' | tr '"' ' ' | tr '\\' ' ')
+        # more recent versions of yq require the following
+        # foods=$(yq '.[].name' ~/.chomp/food_library.yml | paste -sd "," - | tr ',' ' ' | tr '"' ' ' | tr '\\' ' ')
 	COMPREPLY=($(compgen -W '$foods' -- $cur))
 }
 
